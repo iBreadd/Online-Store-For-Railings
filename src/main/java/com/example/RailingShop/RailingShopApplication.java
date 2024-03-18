62d35fec59d9efba6655bbf28d92ec2c1f56cb44
iBreadd <124068827+iBreadd@users.noreply.github.com>
2024-03-18 15:59:05 +0200

    Initial commit

---
+++ src/main/java/com/example/RailingShop/RailingShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.RailingShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailingShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailingShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RailingShop/RailingShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RailingShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailingShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
