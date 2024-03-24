f8bdda81ddd5dfef2d18b92c2be445d9834ee390
Emanuil <eiv.razgrad@gmail.com>
2024-03-24 16:15:32 +0200

    Add entity User, Product.

---
+++ src/main/java/com/example/RailingShop/Entity/Products/Others.java
@@ -0,0 +1,7 @@
+package com.example.RailingShop.Entity.Products;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Others extends Product {
+}
---
+++ src/main/java/com/example/RailingShop/Entity/User/Employee.java
@@ -0,0 +1,48 @@
+package com.example.RailingShop.Entity.User;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee extends User {
+
+    private String first_name;
+
+    private String last_name;
+
+    private Integer age;
+
+    private Double salary;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
