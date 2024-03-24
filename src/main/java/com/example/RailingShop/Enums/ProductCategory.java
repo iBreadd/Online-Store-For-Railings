9bf253e107861535166d7c4ff494f0e0283b906b
Emanuil <eiv.razgrad@gmail.com>
2024-03-24 11:11:11 +0200

    Add enum ProductCategory

---
+++ src/main/java/com/example/RailingShop/Enums/ProductCategory.java
@@ -0,0 +1,16 @@
+package com.example.RailingShop.Enums;
+/*Видовете продукти са следните:
+Храни (food)
+Напитки (drinks)
+Санитарни (sanitary)
+Парапети (railing)
+Аксесоари, декорация, други (others)
+*/
+public enum ProductCategory {
+    FOOD,
+    DRINKS,
+    SANITARY,
+    RAILING,
+    OTHERS
+
+}
